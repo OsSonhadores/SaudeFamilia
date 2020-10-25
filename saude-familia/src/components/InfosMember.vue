718e046d7a2b0e3e991f642f1cb2ee5331c17848
Lucas Simoes <lucassimoessilva@hotmail.com>
2020-10-25 14:18:05 -0300

    Member.vue ready just needing axios for object

--- saude-familia/src/components/InfosMember.vue
+++ saude-familia/src/components/InfosMember.vue
@@ -1,33 +1,92 @@
 <template>
   <div class="container mt-5 mb-5 bg-light rounded">
     <div class="row">
-      <div class="col-12">
+      <div class="col-12 mb-5">
         <b-list-group>
           <b-list-group-item class="li">
-              <div class="col-12">
-                <div class="col-4">
-                  <div class="lil-ball" :style="{ background: person.grade.ballCollor }">
-                    <p class="letter">{{ person.grade.id }}</p>
-                  </div>
-                </div>
-                <div class="float-left col-10">
-                  <p class="description">
-                    {{ person.grade.description }}
-                  </p>
+            <div class="col-12">
+              <div class="col-4">
+                <div
+                  class="lil-ball"
+                  :style="{ background: person.grade.ballCollor }"
+                >
+                  <p class="letter">{{ person.grade.id }}</p>
                 </div>
               </div>
+              <div class="float-left col-10">
+                <p class="description">
+                  {{ person.grade.description }}
+                </p>
+              </div>
+            </div>
           </b-list-group-item>
         </b-list-group>
 
-        <h2 class="display-4 w-100 text-center m-3">{{ person.name }}</h2>
-
-        <div class="w-100">
-            <figure class="w-100">
-                <img :src="'https://s2.glbimg.com/EU1CyKMTp2ELNzpC73Yd_-2soVo=/e.glbimg.com/og/ed/f/original/2019/10/18/faustao01.jpg'" :alt="'{{person.name}}'" class="w-100">
-            </figure>
+        <div class="col-12">
+            <h2 class="display-4 w-100 text-right m-3 mt-5 mr-5">
+              {{ person.name }}
+            </h2>
+            <hr>
         </div>
+      </div>
+    </div>
+  
+    <div class="row">
+      <div class="col-12 col-lg-8">
+          <img :src="person.img" :alt="'{{person.name}}'" class="w-100 rounded border-success" />
 
+        <b-form-checkbox
+          id="priorityCase"
+          v-model="person.priorityCase"
+          name="priorityCase"
+          value="true"
+          unchecked-value="false"
+          class="text-left pt-1 pb-2"
+        >
+          Solicitar atendimento preferêncial
+        </b-form-checkbox>
+      </div>
 
+      <div class="col-12 col-lg-4">
+            <h3 class="w-100 text-center">Informações</h3>
+            <hr>
+        <p>
+          <label for="wheight" class="float-left m-2">Peso Atual</label>
+          <b-form-input
+            id="wheight"
+            v-model="person.wheight"
+            type="number"
+            required
+            class="float-left"
+            style="width: 90px"
+            min="12"
+            max="220"
+          >
+          </b-form-input>
+          <label for="wheight" class="float-left m-2">Kg</label>
+        </p>
+        <br />
+        <br />
+        <p>
+          <label for="height" class="float-left m-2">Altura Atual</label>
+          <b-form-input
+            id="height"
+            v-model="person.height"
+            type="number"
+            required
+            class="float-left"
+            style="width: 90px"
+            min="1.30"
+            max="2.50"
+          >
+          </b-form-input>
+          <label for="height" class="float-left m-2">Metros</label>
+        </p>
+        <br />
+        <br />
+        <p>
+          <b-button class="w-100" variant="outline-success">Salvar</b-button>
+        </p>
       </div>
     </div>
   </div>
@@ -38,17 +97,18 @@ export default {
   data() {
     return {
       person: {
-        img: "khhhhj",
+        img:
+          "https://s2.glbimg.com/EU1CyKMTp2ELNzpC73Yd_-2soVo=/e.glbimg.com/og/ed/f/original/2019/10/18/faustao01.jpg",
         name: "Feitosa",
         wheight: "82",
         height: "1.65",
+        priorityCase: "true",
         grade: {
-          id: "M",
+          id: "G",
           description: "Lorem ipsum",
-          ballCollor: "#dbb60e",
+          ballCollor: "#eb1f00",
         },
       },
-
     };
   },
 };
@@ -56,7 +116,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-
 .letter {
   text-align: center;
   font-size: 50px;
@@ -77,11 +136,10 @@ export default {
   font-size: 24px;
 }
 
-.li{
-    box-shadow: 1px 1px 6px #505050;
-    margin-top: 10px;
+.li {
+  box-shadow: 1px 1px 6px #505050;
+  margin-top: 10px;
 }
-
 
 body {
   background: #13660c !important;
