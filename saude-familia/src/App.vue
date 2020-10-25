f15030fa2e28e91eb13c42119d8c1cb6e20ddb46
Lucas Lima <lucaslim.lux@gmail.com>
2020-10-24 23:14:16 -0300

    aaaaaa

--- saude-familia/src/App.vue
+++ saude-familia/src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <Login/>
   </div>
+  <!-- <router-link to="/about">About</router-link> -->
 </template>
+
+<script>
+import Login from './components/HelloWorld.vue' 
+export default {
+    data () {
+      return {
+        msg: 'Olá Mundo!'
+      }
+    },
+    components:{
+      Login
+    }
+  }
+</script>
 
 <style lang="scss">
 #app {
